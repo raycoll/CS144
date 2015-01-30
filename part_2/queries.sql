8dce47d83a4e6c2dd7a03f70885cfd19dea50e1c
Steven Collison <stevendonv@gmail.com>
2015-01-29 19:49:35 -0800

    adds queries questions :smiley_cat:

---
+++ part_2/queries.sql
@@ -0,0 +1,36 @@
+/* A collection of queries to test the parsed xml data */
+
+/*
+Find the number of users in the database.
+*/
+SELECT count(*)
+FROM AuctionUser;
+
+/*
+ * Find the number of items in "New York", (i.e., items whose location is exactly the string "New York"). 
+ * Pay special attention to case sensitivity. You should match the items in "New York" but not in "new york".
+*/
+
+/*
+ * Find the number of auctions belonging to exactly four categories.
+ */
+
+/*
+ * Find the ID(s) of current (unsold) auction(s) with the highest bid. 
+ * Remember that the data was captured at the point in time December 20th, 2001, one second after midnight, 
+ * so you can use this time point to decide which auction(s) are current. 
+ * Pay special attention to the current auctions without any bid.
+*/
+
+/*
+ * Find the number of sellers whose rating is higher than 1000.
+ */
+
+/*
+ * Find the number of users who are both sellers and bidders.
+ */
+
+/*
+ * Find the number of categories that include at least one item with a bid of more than $100.
+ */
+
