65b397ba56df0263bc796b8a64548b8b6f50eb65
Steven Collison <stevendonv@gmail.com>
2015-02-11 16:56:19 -0800

    ADDS THE MISSING COMMA :incoming_envelope:

--- xml_parsing/create.sql
+++ xml_parsing/create.sql
@@ -9,7 +9,7 @@ CREATE TABLE Item (
     start_price DECIMAL(8,2),
     num_bids INT UNSIGNED NOT NULL,
     latitude DECIMAL(9,6),
-    longitude DECIMAL(9,6)
+    longitude DECIMAL(9,6),
     location VARCHAR(100),
     country VARCHAR(100),
     started TIMESTAMP,
