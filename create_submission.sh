755c2687fc9388186afa93283dda019350ae1023
cs144 <stevendonv@gmail.com>
2015-02-12 20:50:57 -0800

    make create submission create zips in proper folders :sleeping:

--- create_submission.sh
+++ create_submission.sh
@@ -2,15 +2,15 @@
 
 # zip project 2 stuff
 cd xml_parsing
-zip ../project2.zip build.xml create.sql drop.sql load.sql queries.sql runLoad.sh README.txt src/ lib/ 
+zip project2.zip build.xml create.sql drop.sql load.sql queries.sql runLoad.sh README.txt src/ lib/ 
 cd ..
 
 # zip searcher 
 cd searcher
-zip ../project3-searcher.zip build.xml README.txt src/ lib/ WebContents/META-INF/services.xml 
+zip project3-searcher.zip build.xml README.txt src/ lib/ WebContents/META-INF/services.xml 
 cd ..
 
 # zip indexer 
 cd indexer
-zip ../project3-indexer.zip buildSQLIndex.sql dropSQLIndex.sql build.xml README.txt src/ lib/
+zip project3-indexer.zip buildSQLIndex.sql dropSQLIndex.sql build.xml README.txt src/ lib/
 cd .. 
