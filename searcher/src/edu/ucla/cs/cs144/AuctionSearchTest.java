91a0ec69612aebdcd031509ed5c90bd6cbf2debd
synthling <synthialing@Synthias-MacBook-Pro.local>
2015-02-13 18:29:14 -0800

    adds submitted zip and updates AuctionSearchTest

--- searcher/src/edu/ucla/cs/cs144/AuctionSearchTest.java
+++ searcher/src/edu/ucla/cs/cs144/AuctionSearchTest.java
@@ -21,6 +21,8 @@ public class AuctionSearchTest {
 		SearchResult[] basicResults = as.basicSearch(query, 0, 1500);
 		System.out.println("Basic Search Query: " + query);
 		System.out.println("Received " + basicResults.length + " results");
+
+	
 		}
 
 	/*for(SearchResult result : basicResults) {
@@ -35,11 +37,38 @@ public class AuctionSearchTest {
 		for(SearchResult result : spatialResults) {
 			System.out.println(result.getItemId() + ": " + result.getName());
 		}
+
+		SearchResult[] spatialResults2 = as.spatialSearch("superman", region, 0, 200);
+		System.out.println("Spatial Seacrh");
+		System.out.println("Received " + spatialResults2.length + " results");
+		for(SearchResult result2 : spatialResults2) {
+			System.out.println(result2.getItemId() + ": " + result2.getName());
+		}
 		
+		//No bid, no description
 		String itemId = "1497595357";
 		String item = as.getXMLDataForItemId(itemId);
 		System.out.println("XML data for ItemId: " + itemId);
 		System.out.println(item);
+
+		//Multiple bids
+		String itemId2 = "1045776745";
+		String item2 = as.getXMLDataForItemId(itemId2);
+		System.out.println("XML data for ItemId: " + itemId2);
+		System.out.println(item2);
+
+		//No longitude,latitude
+		String itemId3 = "1679455238";
+		String item3 = as.getXMLDataForItemId(itemId3);
+		System.out.println("XML data for ItemId: " + itemId3);
+		System.out.println(item3);
+
+		//Has buyprice
+		String itemId4 = "1679391688";
+		String item4= as.getXMLDataForItemId(itemId4);
+		System.out.println("XML data for ItemId: " + itemId4);
+		System.out.println(item4);
+
 
 		// Add your own test here
 	}
