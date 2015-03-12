0ef8bdf097b098d28159d25f10960ef6e835560c
synthling <synthialing@Synthias-MacBook-Pro.local>
2015-03-11 17:11:37 -0700

    Also added doGet

--- project4/src/edu/ucla/cs/cs144/ConfirmServlet.java
+++ project4/src/edu/ucla/cs/cs144/ConfirmServlet.java
@@ -40,4 +40,31 @@ public class ConfirmServlet extends HttpServlet implements Servlet {
         request.getRequestDispatcher("./confirm.jsp").forward(request, response);
         
     }
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        HttpSession session = request.getSession(true);
+
+        String id = (String)session.getAttribute("id");
+        String name= (String)session.getAttribute("name");
+        String buyPrice= (String)session.getAttribute("buyPrice");
+
+        String cardNum = request.getParameter("cardNum");
+
+        if (!request.isSecure()){
+            request.setAttribute("isSecure", "false");
+        } else if (id == null || name == null || buyPrice == null || cardNum==null) {
+            request.setAttribute("badSession", "true");
+            request.setAttribute("isSecure", "true");
+        } else {
+            request.setAttribute("isSecure", "true");
+            request.setAttribute("badSession", "false");
+            request.setAttribute("id",id);
+            request.setAttribute("name", name);
+            request.setAttribute("buyPrice", buyPrice);
+            request.setAttribute("cardNum",cardNum);
+        }
+
+        request.getRequestDispatcher("./confirm.jsp").forward(request, response);
+        
+    }
 }
